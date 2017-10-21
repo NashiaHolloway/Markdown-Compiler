7cec020bf77099c22258859481fbfa589a3da1ce
NashiaHolloway <nashiaholloway1@gmail.com>
2017-10-21 13:59:30 -0400

    All classes, objects and traits waiting to be coded in :)

---
+++ src/main/scala/edu/towson/cosc/cosc455/nhollo1/project1/CONSTANTS.scala
@@ -0,0 +1,9 @@
+package edu.towson.cosc.cosc455.nhollo1.project1
+
+object CONSTANTS {
+  val DOC_BEGIN : String = 	"\\BEGIN"
+  val DOC_END : String = 	"\\END"
+  val TITLE : String = "\\TITLE"
+  val BRACK_BEGIN : String = "["
+  val BRACK_END : String = "]"
+}
